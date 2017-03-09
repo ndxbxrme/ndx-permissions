e79bb30dbefb4a723cc1c4c120a55d05dcab6e68
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-09 20:56:42 +0000

    hook into database

--- build/index.js
+++ build/index.js
@@ -1,35 +1,77 @@
 (function() {
   'use strict';
-  module.exports = function() {
-    var authenticate, permissions;
+  module.exports = function(ndx) {
+    var check, getTransformer, permissions;
     permissions = {};
-    authenticate = null;
+    check = function(op, table, obj) {
+      var permissionOp, permissionTable;
+      if (permissionTable = permissions[table]) {
+        if (permissionOp = permissionTable[op] || permissionTable['all']) {
+          if (authenticate && authenticate(permissionOp, obj)) {
+            true;
+          } else {
+            throw 'Not authorized';
+          }
+        }
+      }
+      return true;
+    };
+    getTransformer = function(op, table, obj) {
+      var permissionOp, permissionTable;
+      if (permissionTable = permissions[table]) {
+        if (permissionOp = permissionTable[op] || permissionTable['all']) {
+          return permissionOp.transformer;
+        }
+      }
+    };
+    ndx.database.on('preSelect', function(args) {
+      if (!args.isServer) {
+        return check('select', args.table);
+      }
+    });
+    ndx.database.on('select', function(args) {
+      var i, item, len, ref, results, transformer;
+      if (!args.isServer) {
+        if (transformer = getTransformer('select', args.table)) {
+          ref = args.objs;
+          results = [];
+          for (i = 0, len = ref.length; i < len; i++) {
+            item = ref[i];
+            results.push(item = objtrans(item, transformer));
+          }
+          return results;
+        }
+      }
+    });
+    ndx.database.on('preUpdate', function(args) {
+      var transformer;
+      if (!args.isServer) {
+        check('update', args.table, args.obj);
+        if (transformer = getTransformer('update', args.table)) {
+          return args.obj = objtrans(args.obj, transformer);
+        }
+      }
+    });
+    ndx.database.on('preInsert', function(args) {
+      var transformer;
+      if (!args.isServer) {
+        check('insert', args.table, args.obj);
+        if (transformer = getTransformer('insert', args.table)) {
+          return args.obj = objtrans(args.obj, transformer);
+        }
+      }
+    });
+    ndx.database.on('preDelete', function(args) {
+      if (!args.isServer) {
+        return check('delete', args.table);
+      }
+    });
     return {
-      check: function(op, table, user) {
-        var permissionOp, permissionTable;
-        if (permissionTable = permissions[table]) {
-          if (permissionOp = permissionTable[op] || permissionTable['all']) {
-            if (authenticate && authenticate(permissionOp)) {
-              true;
-            } else {
-              throw 'Not authorized';
-            }
-          }
-        }
-        return true;
-      },
-      getTransformer: function(op, table, user) {
-        var permissionOp, permissionTable;
-        if (permissionTable = permissions[table]) {
-          if (permissionOp = permissionTable[op] || permissionTable['all']) {
-            return permissionOp.transformer;
-          }
-        }
-      },
       setPermissions: function(_permissions) {
         permissions = _permissions;
       },
       setAuthenticate: function(authenticateFn) {
+        var authenticate;
         authenticate = authenticateFn;
       }
     };
--- src/index.coffee
+++ src/index.coffee
@@ -1,21 +1,41 @@
 'use strict'
 
-module.exports = () ->
+module.exports = (ndx) ->
   permissions = {}
-  authenticate = null
-  check: (op, table, user) ->
+  check = (op, table, obj) ->
     if permissionTable = permissions[table]
       if permissionOp = (permissionTable[op] or permissionTable['all'])
-        if authenticate and authenticate permissionOp
+        if authenticate and authenticate permissionOp, obj
           true
         else
           throw 'Not authorized'
     true
-  getTransformer: (op, table, user) ->
+  getTransformer = (op, table, obj) ->
     if permissionTable = permissions[table]
       if permissionOp = (permissionTable[op] or permissionTable['all'])
         return permissionOp.transformer
     return
+  ndx.database.on 'preSelect', (args) ->
+    if not args.isServer
+      check 'select', args.table
+  ndx.database.on 'select', (args) ->
+    if not args.isServer
+      if transformer = getTransformer 'select', args.table
+        for item in args.objs
+          item = objtrans item, transformer
+  ndx.database.on 'preUpdate', (args) ->
+    if not args.isServer
+      check 'update', args.table, args.obj
+      if transformer = getTransformer 'update', args.table
+        args.obj = objtrans args.obj, transformer
+  ndx.database.on 'preInsert', (args) ->
+    if not args.isServer
+      check 'insert', args.table, args.obj
+      if transformer = getTransformer 'insert', args.table
+        args.obj = objtrans args.obj, transformer
+  ndx.database.on 'preDelete', (args) ->
+    if not args.isServer
+      check 'delete', args.table
   setPermissions: (_permissions) ->
     permissions = _permissions
     return
